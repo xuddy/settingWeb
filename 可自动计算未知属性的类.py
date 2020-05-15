e8c30e54f95b87b70ca5a600d518fa0c97bef057
xuddy <50688713+xuddy@users.noreply.github.com>
2020-05-15 10:16:41 +0800

    Update 可自动计算未知属性的类.py

--- 可自动计算未知属性的类.py
+++ 可自动计算未知属性的类.py
@@ -1,23 +1,27 @@
-class Root(object):
+class A(object):
 
     def __init__(self, origin):
         self.origin = origin
         self.p1 = None
-        self.p2 = None
+
+    def __set_p(self):
+        self.p1 = self.origin.get("p1")
 
     def set_p(self):
-        self.p1 = self.origin.get("p1")
-        self.p2 = self.origin.get("p2")
+        self.__set_p()
+        print "A.set"
 
 
-class Branch(Root):
+class B(A):
     def __init__(self, origin):
-        super(Branch, self).__init__(origin)
-        super(Branch, self).set_p()
+        super(B, self).__init__(origin)
+        super(B, self).set_p()
+
+    def set_p(self):
+        pass
 
 
 if __name__ == '__main__':
-    a = Root({"p1": "a", "p2": "b"})
-    print a.p1  # 输出为None
-    b = Branch({"p1": "a", "p2": "b"})
-    print b.p1  # 输出为a
+    b = B({"p1": "a"})  # A.set
+    print b.p1   # a
+    b.set_p()   # do nothing
